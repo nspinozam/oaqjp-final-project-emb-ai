efb7a7e4f89abe7f6f4a233b39a1d613a2e95cf8
nspinozam <nspinozam@gmail.com>
2026-05-01 01:15:30 -0400

    initial commit

---
+++ emtion_detection.py
@@ -0,0 +1,36 @@
+import requests
+import json
+
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(URL, json = input_json, headers=header)
+    formated_response = json.loads(response.text)
+
+    if formated_response['emotionPredictions'] is not None:
+        emotions = formated_response['emotionPredictions'][0]['emotion']
+        anger = emotions['anger']
+        disgust = emotions['disgust']
+        fear = emotions['fear']
+        joy = emotions['joy']
+        sadness = emotions['sadness']
+        max_emotion = max(emotions, key=emotions.get)
+        #max_emotion_score = emotions[max_emotion]
+        formated_dict_emotions = {
+                                'anger': anger,
+                                'disgust': disgust,
+                                'fear': fear,
+                                'joy': joy,
+                                'sadness': sadness,
+                                'dominant_emotion': max_emotion
+                                }
+    print(formated_dict_emotions)    
+    return formated_dict_emotions
+
+
+if __name__ == "__main__":
+    emotion_detector("I love this new technology.")
